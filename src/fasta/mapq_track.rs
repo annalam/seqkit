c598a464b9d7f07b653e4e64280f1db5f41301bd
Gnanavel Mutharasu <gnanavel.mutharasu@gmail.com>
2017-10-13 18:02:02 +0300

    modified:   src/fasta/mapq_track.rs

--- src/fasta/mapq_track.rs
+++ src/fasta/mapq_track.rs
@@ -5,8 +5,9 @@ use std::thread;
 use std::sync::{Arc, Mutex};
 use ErrorHelper;
 use std::process::{Command, Stdio};
-use std::io::{BufReader, BufRead, Write};
+use std::io::{BufReader, BufWriter, BufRead, Write};
 use std::collections::HashMap;
+use std::fs::File;
 use bio::io::fasta;
 
 
@@ -23,55 +24,76 @@ Usage:
 pub fn main() {
     let args = parse_args(USAGE);
     let genome_path = args.get_str("<genome>");
-    let win_size: i32 = args.get_str("--win-size").parse().unwrap();
-    let slide_window = args.get_bool("--sliding");
+    let win_size: usize = args.get_str("--win-size").parse().unwrap();
+    let sliding = args.get_bool("--sliding");
 
+    let genome = genome_path.to_owned();
 
-    let fasta = fasta::Reader::from_file(format!("{}.fa", genome_path))
-		.on_error(&format!("Genome FASTA file {}.fa could not be read.", genome_path));
-	eprintln!("Reading reference genome into memory...");
+    let bowtie = Command::new("bowtie")
+        .args(&["-p1", "-a", &genome_path, "-f", "-"])
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped()).spawn()
+        .on_error("Could not start Bowtie process.");
 
-    let mut genome = HashMap::new();
-	for entry in fasta.records() {
-		let chr = entry.unwrap();
-		genome.insert(chr.id().to_owned(), chr.seq().to_owned());
-	}
+    let mut bowtie_in = BufWriter::new(bowtie.stdin.unwrap());
+    let bowtie_out = BufReader::new(bowtie.stdout.unwrap());
 
+    thread::spawn(move || {
+        eprintln!("spawning a new thread for bowtie!");
+        eprintln!("Reading reference genome into memory...");
+        let fasta = fasta::Reader::from_file(format!("{}.fa", &genome))
+    		.on_error(&format!("Genome FASTA file {}.fa could not be read.", &genome));
 
-    // to make chromosome number order sequence
-    let chromosomes = Arc::new(Mutex::new(vec!["chr1", "chr2", "chr3", "chr4", "chr5", "chr6",
-                    "chr7","chr8", "chr9", "chr10", "chr11", "chr12", "chr13", "chr14",
-                    "chr16","chr17", "chr18","chr19","chr20","chr21","chr22","chrX","chrY"]));
-
-
-    // starting from chromosome 1, towards 22 then X and Y
-    //let mut qual = HashMap::new();
-    for n in 0..23 {
-        let chrom = chromosomes.clone();
-        let refg  = genome.clone();
-        let win   = win_size.clone();
-        let geno  = genome_path.to_owned();
-        let mut threads: Vec<i32> = vec![];
-
-        let cur = thread::spawn(move || {
-            let  mut chr = chrom.lock().unwrap();
-            let ch  = chr[n];
-            let seq = refg.get(ch).unwrap();
-            println!("{}\t{}\t{}\t{:?}", ch, seq.len(), win, geno);
-            moving(&ch, &seq, win, geno);
-        });
-
-        /*
-        if slide_window {
-            sliding(&ch, &seq, win_size);
-        } else {
-            moving(&ch, &seq, win_size, ref_genome);
-        }
-        */
-    }
+        if sliding {
+                println!("{}", "Sliding window mode selected!");
+    			send_sliding_windows(fasta, &mut bowtie_in, win_size);
+    		} else {
+                println!("{}", "Moving window mode selected!");
+    			send_moving_windows(fasta, &mut bowtie_in, win_size);
+    		}
+    });
 }
 
 
+fn send_moving_windows(fasta: fasta::Reader<File>, aligner_in: &mut Write, win_size: usize) {
+    println!("running send_moving_windows function");
+    let mut num_reads_sent = 0;
+    let mut strt: usize = 0;
+    let mut endn: usize = 0;
+    println!("Not going beyond this point!");
+    println!("{}\t////////////////////////", fasta.records().count());
+    /*
+    for entry in fasta.records() {
+        let chr = entry.unwrap();
+        println!("{}\t{}", chr.id(), chr.seq().len());
+
+        let ch  = chr.id().to_owned();
+        let seq = chr.seq().to_owned();
+
+        while strt <= seq.len() {
+            endn = strt + win_size as usize;
+            let read = seq.get(strt..endn).unwrap();
+            let window  = ch.to_owned() + ":" + &strt.to_string() + "-" + &endn.to_string();
+
+            println!(">{:?}\n{:?}", window, read);
+            write!(aligner_in, ">{}\n", window);
+    		aligner_in.write_all(read);
+            writeln!(aligner_in);
+            num_reads_sent += 1;
+            strt += win_size as usize;
+        }
+    }
+    */
+    println!("{:?}\t", num_reads_sent);
+}
+
+/// Compute mapping quality for reference genome
+/// using sliding window of given size
+fn send_sliding_windows(chr: fasta::Reader<File>, aligner_in: &mut Write, win_size: usize){
+    unimplemented!()
+}
+
+/*
 /// Compute mapping quality for given read
 /// using bowtie1
 fn align(read: String, genome_path: &str) -> usize {
@@ -115,18 +137,4 @@ fn moving(chr: &str, seq: &[u8], window_size: i32, genome_path: String) {
         strt += window_size as usize;
     }
 }
-
-
-/// Compute mapping quality for reference genome
-/// using sliding window of given size
-fn sliding(chr: &str,  seq: &[u8], window_size: i32){
-    /*
-    let mut qual: Vec<usize> = Vec::new();
-    let mut strt: usize = 0;
-    let mut endn  = strt + window_size as usize;
-    let read = seq.get(strt..endn).unwrap();
-
-    println!("{:?}", read.len());
-    */
-    unimplemented!()
-}
+*/

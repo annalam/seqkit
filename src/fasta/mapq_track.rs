7288a21f5380af06eee9e67f28ec1e34c7ae1851
Gnanavel Mutharasu <gnanavel.mutharasu@gmail.com>
2017-10-13 11:28:35 +0300

    modified:   src/fasta/mapq_track.rs

--- src/fasta/mapq_track.rs
+++ src/fasta/mapq_track.rs
@@ -39,7 +39,6 @@ pub fn main() {
 
 
     // to make chromosome number order sequence
-
     let chromosomes = Arc::new(Mutex::new(vec!["chr1", "chr2", "chr3", "chr4", "chr5", "chr6",
                     "chr7","chr8", "chr9", "chr10", "chr11", "chr12", "chr13", "chr14",
                     "chr16","chr17", "chr18","chr19","chr20","chr21","chr22","chrX","chrY"]));

c056c9196783bbda7e6af6158b69d7790bb07648
mgvel <gnanavel.mutharasu@gmail.com>
2017-10-20 18:37:40 +0300

    input list function added

--- src/fasta/mappability_track.rs
+++ src/fasta/mappability_track.rs
@@ -5,26 +5,43 @@ use std::thread;
 use ErrorHelper;
 use std::process::{Command, Stdio};
 use std::io::{BufReader, BufWriter, BufRead, Write};
+use std::collections::HashSet;
 use std::fs::File;
 use bio::io::fasta;
+
 
 const USAGE: &'static str = "
 Usage:
   fasta mappability track [options] <genome>
 
  Options:
-    --win-size=N     window size for bowtie alignment (4-1024) [default: 48]
-    --sliding        enable sliding window mode
+    --win-size=N    window size for bowtie alignment (4-1024) [default: 48]
+    --sliding       enable sliding window mode
+    --list=PATH     File containing list of chromosome positions
 ";
 
 
 pub fn main() {
     let args = parse_args(USAGE);
-    let genome_path = args.get_str("<genome>");
+    let genome_path     = args.get_str("<genome>");
     let win_size: usize = args.get_str("--win-size").parse().unwrap();
-    let sliding = args.get_bool("--sliding");
+    let sliding         = args.get_bool("--sliding");
+    let list_path       = args.get_str("--list").to_owned();
 
     let genome = genome_path.to_owned();
+
+    let mut list_pos = Vec::<String>::new();
+    if !list_path.is_empty() {
+        let ls = BufReader::new(File::open(&list_path).on_error(
+            &format!("Could not open list file '{}'.", list_path)));
+
+        for l in ls.lines() {
+            let line = l.unwrap().to_string();
+            list_pos.push(line);
+        }
+    }
+
+
     let bowtie = Command::new("bowtie")
         // bowtie running in a single thread to maintain the sequence order[-p1]
         // maximum of 10 aligments are considered for every input sequence slice[-k10]
@@ -41,8 +58,11 @@ pub fn main() {
         eprintln!("Reading reference genome into memory...");
         let fasta = fasta::Reader::from_file(format!("{}.fa", &genome))
     		.on_error(&format!("Genome FASTA file {}.fa could not be read.", &genome));
-
-        send_seq_slices(fasta, &mut bowtie_in, win_size, sliding);
+        if !list_path.is_empty() {
+            send_list_slices(fasta, &mut bowtie_in, list_pos, win_size);
+        } else {
+            send_seq_slices(fasta,  &mut bowtie_in, win_size, sliding);
+        }
     });
 
     let mut prev = String::new();
@@ -96,7 +116,12 @@ fn send_seq_slices(fasta: fasta::Reader<File>, aligner_in: &mut Write, win_size:
             let endn = strt + win_size as usize;
             let read = seq.get(strt..endn).unwrap();
             // printing genome slice into 1-based co-ordinates
-            let window  = ch.to_owned() + ":" + &strt.to_string() + "-" + &endn.to_string();
+            let mut window = String::new();
+            if sliding {
+                window  = ch.to_owned() + ":" + &strt.to_string();
+            } else {
+                window  = ch.to_owned() + ":" + &strt.to_string() + "-" + &endn.to_string();
+            }
 
             //println!(">{}\n{}", &window, String::from_utf8(read.to_owned()).unwrap());
         	write!(aligner_in, ">{}:\n", &window);
@@ -105,6 +130,36 @@ fn send_seq_slices(fasta: fasta::Reader<File>, aligner_in: &mut Write, win_size:
                 strt += 1;
             } else {
                 strt += win_size as usize;
+            }
+        }
+        eprintln!("Processing {}\tcompleted!", &ch);
+    }
+}
+
+
+/// compute mappbility score for list of input chromosome positions
+/// each line in input line should be in chrX:NNNNNNN format
+fn send_list_slices(fasta: fasta::Reader<File>, aligner_in: &mut Write, list_pos: Vec<String>, win_size: usize) {
+
+    for entry in fasta.records() {
+        let chr = entry.unwrap();
+        eprintln!("{}\t{}", chr.id(), chr.seq().len());
+        let ch  = chr.id().to_owned();
+        let seq = chr.seq().to_owned();
+
+        for line in list_pos.iter() {
+            let chr         = line.split(':').nth(0).unwrap().to_string();
+            let pos: usize  = line.split(':').nth(1).unwrap().parse().unwrap();
+            let mut strt: usize = pos - (win_size / 2) ;
+
+            if ch == chr && strt + win_size <= seq.len() {
+                let endn = strt + win_size as usize;
+                let read = seq.get(strt..endn).unwrap();
+                // printing genome slice into 1-based co-ordinates
+                let window  = ch.to_owned() + ":" + &strt.to_string();
+                //println!(">{}\n{}", &window, String::from_utf8(read.to_owned()).unwrap());
+                write!(aligner_in, ">{}:\n", &window);
+                aligner_in.write_all(&read);
             }
         }
         eprintln!("Processing {}\tcompleted!", &ch);

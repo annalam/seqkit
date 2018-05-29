498201ff70d8d1ba71b729f352b3864de1fd09ce
annalam <annalam@users.noreply.github.com>
2018-05-29 14:19:59 +0300

    Fixed a bug that caused 'fasta trim by quality' to crash if it encountered an empty sequence in a FASTQ file.

--- src/fasta_trim_by_quality.rs
+++ src/fasta_trim_by_quality.rs
@@ -2,7 +2,6 @@
 use common::{parse_args, FileReader};
 use std::str;
 use std::process::exit;
-use ascii::AsciiString;
 
 const USAGE: &str = "
 Usage:
@@ -12,43 +11,41 @@ Usage:
 pub fn main() {
 	let args = parse_args(USAGE);
 	let mut fasta_file = FileReader::new(&args.get_str("<fastq_file>"));
-	let min_baseq: i32 = args.get_str("<min_baseq>").parse().unwrap();
+	let min_baseq: u8 = args.get_str("<min_baseq>").parse().unwrap();
+	let baseq_offset = 33u8;  // TODO: Support other base quality ASCII offsets
 
-	let mut line = AsciiString::new();
-	while fasta_file.read_ascii_line(&mut line) {
-		if line[0] != '@' {
-			eprintln!("Invalid FASTQ format encountered."); exit(-1);
+	let mut line = String::new();
+	let mut seq = String::new();
+	let mut qual = String::new();
+	while fasta_file.read_line(&mut line) {
+		if !line.starts_with('@') {
+			error!("Invalid FASTQ format encountered.");
 		}
 		print!("{}", line);
-		fasta_file.read_ascii_line(&mut line);
-		let seq = line.clone();
-		fasta_file.read_ascii_line(&mut line);
-		fasta_file.read_ascii_line(&mut line);
-		let qual = &line;
+		fasta_file.read_line(&mut seq);
+		fasta_file.read_line(&mut line);
+		fasta_file.read_line(&mut qual);
 
 		let mut total: i32 = -50;
 		let mut lowest_total: i32 = total;
 
-		let mut k: usize = qual.len() - 1;
-		if qual[k] == '\n' { k -= 1; }
-		let mut lowest_k = k + 1;
-		loop {
-			// FIXME: We assume Sanger format base qualities here.
-			total += (qual[k] as i32 - 33 as i32) - min_baseq;
+		let mut k: usize = qual.trim_right().len();
+		let mut lowest_k = k;
+		while k > 0 {
+			k -= 1;
+			total += (qual.as_bytes()[k] - baseq_offset) as i32 -
+				min_baseq as i32;
 			if total > 0 { break; }
 			if total < lowest_total {
 				lowest_total = total;
 				lowest_k = k;
 			}
-			if k == 0 { break; }
-			k -= 1;
 		}
 
 		if lowest_k == 0 {
-			print!("N\n+\n!\n");
+			print!("N\n+\n!\n");   // Entire read was garbage
 		} else {
 			print!("{}\n+\n{}\n", &seq[..lowest_k], &qual[..lowest_k]);
 		}
-
 	}
 }

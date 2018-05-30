51bb33422885da9f4d0efe53c5ab6f2e04933ee8
annalam <annalam@users.noreply.github.com>
2018-05-30 12:57:16 +0300

    Reverse complement extracted read sequences if they were aligned against minus strand in BAM file.

--- src/sam_to_fastq.rs
+++ src/sam_to_fastq.rs
@@ -11,17 +11,33 @@ Usage:
   sam to fastq <bam_file> <out_prefix>
 ";
 
+
+
 fn sequence(read: &Record) -> String {
 	let seq = read.seq();
 	let qual = read.qual();
 	let mut ret = String::with_capacity(seq.len());
-	for k in 0..seq.len() {
-		if qual[k] < 10 {
-			ret.push('N');
-		} else {
-			ret.push(match seq.encoded_base(k) {
-				1 => 'A', 2 => 'C', 4 => 'G', 8 => 'T', _ => 'N'
-			});
+	if read.is_reverse() {
+		// Output reverse complement of the sequence stored in BAM file.
+		for k in (0..seq.len()).rev() {
+			if qual[k] < 10 {
+				ret.push('N');
+			} else {
+				ret.push(match seq.encoded_base(k) {
+					1 => 'T', 2 => 'G', 4 => 'C', 8 => 'A', _ => 'N'
+				});
+			}
+		}
+	} else {
+		// Output the sequence as it is stored in the BAM file.
+		for k in 0..seq.len() {
+			if qual[k] < 10 {
+				ret.push('N');
+			} else {
+				ret.push(match seq.encoded_base(k) {
+					1 => 'A', 2 => 'C', 4 => 'G', 8 => 'T', _ => 'N'
+				});
+			}
 		}
 	}
 	ret
@@ -48,8 +64,6 @@ pub fn main() {
 	let mut reads_1: HashMap<String, Box<str>> = HashMap::new();
 	let mut reads_2: HashMap<String, Box<str>> = HashMap::new();
 
-	let mut total_reads = 0;
-
 	for r in bam.records() {
 		let mut read = r.unwrap_or_else(
 			|_| error!("Input BAM file ended prematurely."));
@@ -58,21 +72,18 @@ pub fn main() {
 		let qname = str::from_utf8(read.qname()).unwrap();
 
 		if read.is_paired() == false {
-			total_reads += 1;
-			write!(fastq_single, ">{}\n{}\n", total_reads, sequence(&read));
+			write!(fastq_single, ">{}\n{}\n", qname, sequence(&read));
 		} else if read.is_first_in_template() {
 			if let Some(mate_seq) = reads_2.remove(qname) {
-				total_reads += 1;
-				write!(fastq_1, ">{}\n{}\n", total_reads, sequence(&read));
-				write!(fastq_2, ">{}\n{}\n", total_reads, mate_seq);
+				write!(fastq_1, ">{}\n{}\n", qname, sequence(&read));
+				write!(fastq_2, ">{}\n{}\n", qname, mate_seq);
 			} else {
 				reads_1.insert(qname.into(), sequence(&read).into());
 			}
 		} else if read.is_last_in_template() {
 			if let Some(mate_seq) = reads_1.remove(qname) {
-				total_reads += 1;
-				write!(fastq_1, ">{}\n{}\n", total_reads, mate_seq);
-				write!(fastq_2, ">{}\n{}\n", total_reads, sequence(&read));
+				write!(fastq_1, ">{}\n{}\n", qname, mate_seq);
+				write!(fastq_2, ">{}\n{}\n", qname, sequence(&read));
 			} else {
 				reads_2.insert(qname.into(), sequence(&read).into());
 			}

9abedd2abb325a56d394d87eb9edafee1c82d008
annalam <annalam@users.noreply.github.com>
2018-06-26 14:05:21 +0300

    Changed the 'fasta add barcode' and 'fasta demultiplex' commands so that we can demultiplex sequencing runs that contain both UMI and non-UMI samples.

--- src/fasta_demultiplex.rs
+++ src/fasta_demultiplex.rs
@@ -7,6 +7,11 @@ use regex::Regex;
 const USAGE: &str = "
 Usage:
   fasta demultiplex <sample_sheet> <fastq_1> <fastq_2>
+
+Description:
+Splits a pooled FASTQ file into multiple individual FASTQ files, based on a
+sample sheet. Each read in the pooled FASTQ file must carry a BC:xxxxxxxx
+field in its header.
 ";
 
 struct Sample {
@@ -22,16 +27,23 @@ pub fn main() {
 	let mut fastq1 = FileReader::new(&args.get_str("<fastq_1>"));
 	let mut fastq2 = FileReader::new(&args.get_str("<fastq_2>"));
 
-	let barcode_regex = Regex::new(r" SI:[ACGTNacgtn]+").unwrap();
+	let barcode_regex = Regex::new(r" BC:[ACGTNacgtn]+").unwrap();
 
 	// Read the user-provided sample sheet into memory.
 	let mut samples = Vec::new();
 	let mut line = String::new();
+	let mut barcode_len: usize = 0;
 	while sample_sheet.read_line(&mut line) {
 		if line.starts_with('#') { continue; }
 		let cols: Vec<&str> = line.trim().split('\t').collect();
 		if cols.len() < 2 { continue; }
 		let name = cols[0];
+		if cols[1].is_empty() { error!("Sample {} has no barcode.", name); }
+		if barcode_len == 0 {
+			barcode_len = cols[1].len();
+		} else if cols[1].len() != barcode_len {
+			error!("Barcodes in sample sheet must be of same length.");
+		}
 		samples.push(Sample {
 			name: name.into(),
 			barcode: cols[1].into(),
@@ -49,9 +61,9 @@ pub fn main() {
 		}
 	}
 
-	/*let barcode_len = 8;
-	let mut barcode_to_sample = vec![0; 4.pow(8)];
-	for b in 0..4.pow(8) {
+	// TODO: Build a lookup table for (barcode -> sample) mappings.
+	/*let mut barcode_to_sample = vec![0; 4.pow(barcode_len)];
+	for b in 0..4.pow(barcode_len) {
 		for base in 0..barcode_len {
 			
 		}
@@ -63,22 +75,26 @@ pub fn main() {
 	let mut line1 = String::new();
 	let mut line2 = String::new();
 	let mut barcode = String::new();
+	let mut umi = String::new();
 	while fastq1.read_line(&mut line1) && fastq2.read_line(&mut line2) {
 		if line1.starts_with('@') && line2.starts_with('@') {
 
-			// Find the sample barcode, formatted as SI:xxxx.
+			// Find the sample barcode, formatted as BC:xxxx.
 			// Then remove it from the header.
-			// TODO: Allow user to choose if SI:xxxx should be removed.
+			// TODO: Allow user to choose if BC:xxxx should be removed.
 			let (start, end) = {
 				let hit = &barcode_regex.find(&line1)
-					.unwrap_or_else(|| error!("No SI:xxxx field found."));
+					.unwrap_or_else(|| error!("No BC:xxxx field found."));
 				(hit.start(), hit.end())
 			};
 			barcode.clear();
 			barcode += &line1[(start+4)..end];
 			line1.drain(start..end);
+			if barcode.len() != barcode_len {
+				error!("Barcode {} is of different length than barcodes in the sample sheet.", barcode);
+			}
 
-			// Remove SI:xxxx field from the second mate as well (if present).
+			// Remove BC:xxxx field from the second mate as well (if present).
 			let (start, end) = if let Some(hit) = barcode_regex.find(&line2) {
 				(hit.start(), hit.end())
 			} else {
@@ -89,8 +105,19 @@ pub fn main() {
 			if let Some(sample) = samples.iter_mut()
 				.find(|s| barcode_matches(&barcode, &s.barcode)) {
 
-				write!(sample.output1, "{}", line1);
-				write!(sample.output2, "{}", line2);
+				// Extract UMI, if present
+				umi.clear();
+				for x in sample.barcode.chars().zip(barcode.chars()) {
+					if x.0 == 'U' { umi.push(x.1); }
+				}
+
+				write!(sample.output1, "{}", line1.trim_right());
+				if !umi.is_empty() { write!(sample.output1, " UMI:{}", umi); }
+				write!(sample.output1, "\n");
+
+				write!(sample.output2, "{}", line2.trim_right());
+				if !umi.is_empty() { write!(sample.output2, " UMI:{}", umi); }
+				write!(sample.output2, "\n");
 
 				for _ in 0..3 {
 					fastq1.read_line(&mut line1);
@@ -100,7 +127,7 @@ pub fn main() {
 				}
 				identified_reads += 2;
 			} else {
-				// Must read all four lines even if we do not recognize the
+				// Must read all four lines even if we did not recognize the
 				// barcode.
 				for _ in 0..3 { 
 					fastq1.read_line(&mut line1);
@@ -117,11 +144,9 @@ pub fn main() {
 		(identified_reads as f64) / (total_reads as f64) * 100.0);
 }
 
-fn barcode_matches(observed: &str, provided: &str) -> bool {
-	if observed.len() != provided.len() {
-		error!("Barcode {} is of unexpected length.", observed);
-	}
-	observed.chars().zip(provided.chars()).all(|x| x.0 == x.1 || x.1 == 'N')
+fn barcode_matches(observed: &str, candidate: &str) -> bool {
+	observed.chars().zip(candidate.chars()).all(
+		|x| x.0 == x.1 || x.1 == 'N' || x.1 == 'U')
 }
 
 /*

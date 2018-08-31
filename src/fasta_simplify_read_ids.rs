0d595bc584973fa106f6e22104b9b311963fa266
Anssi Nurminen <nurmians@users.noreply.github.com>
2018-08-31 15:20:44 +0300

    fasta_simplify_reads now generates more compact alphanumeric idenfiers (0-9,A-Z,a-z). Changed vector from char to u8.

--- src/fasta_simplify_read_ids.rs
+++ src/fasta_simplify_read_ids.rs
@@ -7,13 +7,12 @@ Usage:
   fasta simplify read ids <fastq_file>
 ";
 
-
-static IDCHARS: [char;62] = ['0','1','2','3','4','5','6','7','8','9',
-						     'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', 
-						     'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' ];
+static IDCHARS: [u8;62] = [  b'0',b'1',b'2',b'3',b'4',b'5',b'6',b'7',b'8',b'9',
+						     b'A',b'B',b'C',b'D',b'E',b'F',b'G',b'H',b'I',b'J',b'K',b'L',b'M',b'N',b'O',b'P',b'Q',b'R',b'S',b'T',b'U',b'V',b'W',b'X',b'Y',b'Z', 
+						     b'a',b'b',b'c',b'd',b'e',b'f',b'g',b'h',b'i',b'j',b'k',b'l',b'm',b'n',b'o',b'p',b'q',b'r',b's',b't',b'u',b'v',b'w',b'x',b'y',b'z' ];
 static NCHARS: usize = 62;
 
-pub fn next_id( id : &mut Vec<usize>, str_id : &mut Vec<char>) {
+pub fn next_id( id : &mut Vec<usize>, str_id : &mut Vec<u8>) {
 
 	let mut i = id.len(); // index of running identifier char
 	
@@ -22,6 +21,7 @@ pub fn next_id( id : &mut Vec<usize>, str_id : &mut Vec<char>) {
         i -= 1;
     
     	loop  {
+    
     		let running_num = id[ i];
     
     		if running_num == NCHARS-1 {
@@ -31,7 +31,8 @@ pub fn next_id( id : &mut Vec<usize>, str_id : &mut Vec<char>) {
     			if i == 0 { break; }
     			i -= 1;
     			continue;
-    		}		
+    		}
+    		
     		// Replace running char with next
     		id[ i] = running_num+1;
     		str_id[ i] = IDCHARS[ running_num+1];
@@ -49,22 +50,21 @@ pub fn main() {
 
 	let mut line = String::new();
 
-	let mut str_id : Vec<char> = Vec::new();
+	let mut str_id : Vec<u8> = Vec::new();
 	let mut running_id : Vec<usize> = Vec::new();
 
 	while fasta_file.read_line(&mut line) {
 		
 		next_id( &mut running_id, &mut str_id);
-        let s: String = str_id.iter().collect();		
 
 		if line.starts_with('@') {
-			println!("@{}", s);
+			println!("@{}", str::from_utf8(&str_id).unwrap());
 			for _ in 0..3 {
 				fasta_file.read_line(&mut line);
 				print!("{}", line);
 			}
 		} else if line.starts_with('>') {
-			println!(">{}", s);
+			println!(">{}", str::from_utf8(&str_id).unwrap());
 			fasta_file.read_line(&mut line);
 			print!("{}", line);
 		} else {

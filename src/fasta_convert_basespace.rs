7f9847cceb3e0b73499ed9545bf4ae1f46e0c2ed
annalam <annalam@users.noreply.github.com>
2018-09-25 13:53:38 +0300

    Added some documentation for 'fasta convert basespace'.

--- src/fasta_convert_basespace.rs
+++ src/fasta_convert_basespace.rs
@@ -6,17 +6,31 @@ use std::io::Write;
 const USAGE: &str = "
 Usage:
   fasta convert basespace <fastq_file>
+
+Description:
+FASTQ files from Illumina Basespace typically display adapter barcodes at
+the end of the FASTQ header, and have read identifiers that end in /1 or /2.
+This tool replaces the read identifiers by simple consecutive integers, and
+places a \"BC:\" prefix in front of the barcode. An example FASTQ header in
+the output could look like this: @412435 BC:TAGCTACT
 ";
 
 pub fn main() {
 	let args = parse_args(USAGE);
 	let mut fastq = FileReader::new(&args.get_str("<fastq_file>"));
 
+	let mut num_read_pairs: u64 = 0;
+
 	let mut header = String::new();
 	let mut line = String::new();
 	while fastq.read_line(&mut header) == true {
+		num_read_pairs += 1;
+		print!("@{}", num_read_pairs);
+
+		// TODO: Add option for keeping the original fragment ID.
+		// print!("{}", header.split(' ').next().unwrap());
+
 		let barcode = header.trim_right().split(':').last().unwrap_or("");
-		print!("{}", header.split(' ').next().unwrap());
 		if !barcode.is_empty() { print!(" BC:{}", barcode); }
 		println!();
 
